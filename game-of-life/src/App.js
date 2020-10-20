830f3a51e237713103a7d5c54bf195426d4598fa
Heber <hebergonza@gmail.com>
2020-10-20 02:28:17 -0400

    clear button working, deleted hooks folder

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from './Grid';
 import './App.css';
 
 function App() {
-  const defaults = {
+  const [refresh, setRefresh] = useState(false);
+
+  const dimension = {
     row: 25,
     col: 25
   }
 
-  let matrix = new Array(defaults.row);
+  let matrix = new Array(dimension.row);
 
   for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = new Array(defaults.col);
+    matrix[i] = new Array(dimension.col);
     for (let y = 0; y < matrix[i].length; y++) {
       matrix[i][y] = false;
     };
   };
 
+  const toggleClear = (e) => {
+    e.preventDefault();
+    for (let x = 0; x < matrix.length; x++) {
+      for (let y = 0; y < matrix[x].length; y++) {
+        matrix[x][y] = false;
+      };
+    };
+    setRefresh(!refresh)
+  };
+
+  useEffect(() => {
+
+  }, [refresh])
+
   return ( 
-    <div>
-      <Grid matrix={matrix}/>
-    </div>
+      <div>
+        <Grid matrix={matrix}/>
+        <button onClick={toggleClear}>Clear</button>
+      </div>
   )
 }
 
